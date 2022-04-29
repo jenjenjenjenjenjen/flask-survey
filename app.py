b523d99a46e2a2cf910a0bc1f1dee15c7443938c
Jenny <jenny@Jennys-MacBook-Pro.local>
2022-04-29 16:34:50 -0600

    adding sessions

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from crypt import methods
 from urllib import response
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import *
 from flask_debugtoolbar import DebugToolbarExtension
+
+RESPONSES_KEY = "responses"
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='ketchup'
 debug = DebugToolbarExtension(app)
 
-responses = []
+
 
 @app.route('/')
 def welcome_page():
@@ -16,11 +18,18 @@ def welcome_page():
     instructions = satisfaction_survey.instructions
     return render_template('base.html', instructions=instructions, title=title)
 
+@app.route('/begin', methods=['POST'])
+def start_survey():
+    session[RESPONSES_KEY] = []
+    return redirect('/questions/0')
+
+
 num = 0
 length = len(satisfaction_survey.questions)
 
 @app.route('/questions/<int:num>')
 def show_question(num):
+    responses = session.get(RESPONSES_KEY)
     question = satisfaction_survey.questions[num].question
     choices = satisfaction_survey.questions[num].choices
     if responses is None:
@@ -33,7 +42,9 @@ def show_question(num):
 @app.route('/answer/<int:num>', methods=['POST'])
 def answer_page(num):
     result = request.form.getlist('choice')
+    responses = session[RESPONSES_KEY]
     responses.append(result)
+    session[RESPONSES_KEY] = responses
     num += 1
     if num == length:
         return redirect('/thank-you')
